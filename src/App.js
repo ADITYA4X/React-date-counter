43f12b8fce9705865be0962bd5994ee98e48af32
aditya <adityakunghadkar94.com>
2023-12-06 01:06:48 +0530

    File Updated

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ export default function App() {
 function Counter() {
   const [count, setCount] = useState(0);
 
+  const date = new Date();
+  date.setDate(date.getDate() + count);
 
   return <div>
     <div>
@@ -20,6 +22,13 @@ function Counter() {
         <span>Count: {count}</span>
         <button className="buttons" onClick={()=> setCount((c) => c + 1)}>+</button>
       </div>
+
+      <p> <span>{count === 0 ? "Today is " : count > 0 ? `${count} days from today is ` : `${Math.abs(count)} days ago was `} </span>
+
+        <span>{date.toDateString()}</span>
+      </p>
+
+
     </div>
   </div>
 }
